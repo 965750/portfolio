c9b2f24cdac6f79824beabefbd009cacc2d19a5d
965750 <aventhelast@gmail.com>
2018-03-14 15:13:06 +0100

    new menu fixed bar

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,43 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    
+    let nav = 0;
+    $(".btnNav").on("click", function () {
+        if (nav == 0) {
 
-let nav = 0;    
-    $(".btnNav").on("click", function(){
-        if(nav == 0){
-            
             $(".navOpt").animate({
                 right: '0px'
-            },200);
-        
+            }, 200);
+
             nav = 1;
-        } 
-        else 
-        {
-            
+        } else {
+
             $(".navOpt").animate({
                 right: '-200px'
-            },400);
-            
+            }, 400);
+
             nav = 0;
+        }
+    });
+    if($(this).scrollTop() >= 200){
+        $(".banerCont").css({
+                opacity: '0'
+            });
+    }
+    $(window).on("scroll", function(){
+    //banerTOP
+        if($(this).scrollTop() >= 200){
+            $(".banerCont").css({
+                opacity: '-=.18'
+            });
+        } else if($(this).scrollTop() == 0){
+            $(".banerCont").css({
+                opacity: '1'
+            });
+        }
+        else {
+            $(".banerCont").css({
+                opacity: '+=.5'
+            });
         }
     });
 });
---
+++ src/js/mainBar.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+
+    $(".barContRight").hide();
+    //hover
+    $(".barContLeft").on("mouseenter", function () {
+        $(this).next().fadeToggle(120);
+    });
+    $(".barContLeft").on("mouseleave", function () {
+        $(this).next().fadeToggle(70);
+    });
+    var panel = 0;
+    //click
+    $(".barContLeft").on("click", function () {
+
+        $(this).prev().toggleClass("leftPanelActive");
+        $(".barContAll").toggleClass("barContAllActive");
+        $(".barContLeft").not(this).fadeToggle(50);
+
+        //panel ON
+        if (panel == 0) {
+            $(this).append("<i class='fas fa-times iconFA ys'></i>");
+            $(this).find(".nc").fadeOut(0);
+            $(".barContRight").css({
+                width: "0px",
+                opacity: 0
+            });
+            $(this).css({
+                borderTopRightRadius: "10px",
+                borderBottomRightRadius: "10px"
+            });
+
+             $(".barContLeft").on("mouseenter", function () {
+                $(this).next().children().css({
+                    color: 'transparent'
+                });
+            });
+            $(".barContLeft").on("mouseleave", function () {
+                $(this).next().children.css({
+                    color: 'transparent'
+                });
+            });
+            
+            panel = 1;
+
+            //panel OFF    
+        } else {
+            $(this).find(".ys").hide(0);
+            $(this).find(".nc").fadeIn(0);
+            
+            $(".barContRight").css({
+                width: "100px",
+                opacity: 1
+            });
+            
+            $(this).css({
+                borderTopRightRadius: "0px",
+                borderBottomRightRadius: "0px"
+            });
+            
+            $(".barContLeft").on("mouseenter", function () {
+                $(this).next().children().css({
+                    color: 'white'
+                });
+            });
+
+            panel = 0;
+        }
+    });
+
+});
